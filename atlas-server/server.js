2b98348a06d467bb64c5775f036d7f0920681a76
samismos <samismos@yahoo.gr>
2024-08-31 02:01:23 +0300

    Use env variables in CORS config

--- atlas-server/server.js
+++ atlas-server/server.js
@@ -7,29 +7,25 @@ const PORT = process.env.PORT || 3100;
 
 app.use(express.json());
 
-/*
-const allowedOrigins = [
-  'http://192.168.0.71:3000',
-  'http://localhost:3000',
-  'http://192.168.0.132:3000' 
-];
-*/
 
-/*
 const corsOptions = {
-  origin: (origin, callback) => {
-    if (allowedOrigins.includes(origin)) {
+  origin: function (origin, callback) {
+    const allowedOrigins = [process.env.ALLOWED_ORIGIN_1, "http://localhost"]; 
+    if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
       callback(new Error('Not allowed by CORS'));
     }
   },
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  allowedHeaders: 'Content-Type,Authorization',
+  credentials: true
 };
-*/
 
-const corsOptions = {
-  origin: '*',
-}
+// const corsOptions = {
+//   origin: '*',
+// }
+
 app.use(cors(corsOptions));
 
 app.get('/api/get-country-flag', async (req, res) => {
@@ -40,15 +36,15 @@ app.get('/api/get-country-flag', async (req, res) => {
     }
     // Step 1: Get the 2-letter code based on selected country
     const countryCodeResponse = await axios.get(`https://restcountries.com/v3.1/name/${selectedCountry}?fields=name,cca2,population,flags`);
-    console.log(countryCodeResponse.data);  
+    // console.log(countryCodeResponse.data);  
     const countryCode = countryCodeResponse.data[0]?.cca2;
     const flagLink = countryCodeResponse.data[0]?.flags.svg;
     const flagAlt = countryCodeResponse.data[0]?.flags.alt;
     const population = countryCodeResponse.data[0]?.population;
 
     // Send the country data to the client
-    const package = {countryCode, flagLink, flagAlt, population};
-    res.send(package);
+    const response = {countryCode, flagLink, flagAlt, population};
+    res.send(response);
 
     //res.send(flagResponse.data);
 
